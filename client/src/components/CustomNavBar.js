614e63e0bc4f7b0c679e13e02d6d237ad943f90a
BCT <bertrand.chevalier@gmail.com>
2020-08-23 15:16:16 -0400

    Removed some commented code

--- client/src/components/CustomNavBar.js
+++ client/src/components/CustomNavBar.js
@@ -13,7 +13,6 @@ class CustomNavBar extends Component {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                        {/* <Nav.Link href="/">Home</Nav.Link> */}
                         <Nav.Link href="/mealplan">Mealplan</Nav.Link>
                         <Nav.Link href="/shopping bag">My Cart</Nav.Link>
                         <Nav.Link href="/browse">Browse</Nav.Link>
@@ -21,9 +20,6 @@ class CustomNavBar extends Component {
                             <NavDropdown.Item href="/suggest a recipe">Suggest a recipe</NavDropdown.Item>
                             <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
                             <NavDropdown.Item href="/settings">Settings</NavDropdown.Item>
-                            {/* <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item> */}
-                            {/* <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
                         </NavDropdown>
                         </Nav>
                         <Form inline>
