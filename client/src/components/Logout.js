4e791e8c9aa78a56b00e389d5c5f7bfd566b001a
BCT <bertrand.chevalier@gmail.com>
2020-08-25 14:04:41 -0400

    Created Shell for Logout and UserPrefences components

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {withRouter} from 'react-router-dom';
+import Button from 'react-bootstrap/Button'
 
 class Logout extends Component {
     
@@ -8,7 +9,7 @@ class Logout extends Component {
 
         return(
             <div className="logout">
-                Logout
+                <Button>Logout</Button>
             </div>
             )
         }
--- client/src/components/UserPreferences.js
+++ client/src/components/UserPreferences.js
@@ -1,14 +1,65 @@
 import React, { Component } from 'react';
 import {withRouter} from 'react-router-dom';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 class UserPreferences extends Component {
     
+    constructor(props){
+        super(props)
+        this.state={
+            defaultNumberOfGuests: 2,
+            days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+        }
+    }
 
+    renderDaysOptions(){
+        return(
+            this.state.days.map((day, key)=>{
+                return(
+                    <option key={key}>{day}</option>
+                )
+            })
+        )
+    }
     render() {
 
         return(
             <div className="userPreferences">
-                UserPreferences
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Default Number of guests</Form.Label>
+                        <Form.Control type="text" placeholder={this.state.defaultNumberOfGuests} />
+                    </Form.Group>
+
+                    <Form.Group controlId="formGridState">
+                        <Form.Label>Prefered first day of the week</Form.Label>
+                        <Form.Control as="select" defaultValue="Choose...">
+                            {this.renderDaysOptions()}
+                        </Form.Control>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicCheckbox">
+                        <br/>
+                        <div>Diet Restrictions</div>
+                        {/* <br/> */}
+                        <Form.Check type="checkbox" label="Gluten free" />
+                        <Form.Check type="checkbox" label="Lactose intolerant" />
+                        <Form.Check type="checkbox" label="Vegetarian" />
+                        <Form.Check type="checkbox" label="Vegan" />
+                        
+                        <br/>
+                        <div>Allergies</div>
+                        {/* <br/> */}
+                        <Form.Check type="checkbox" label="Nuts" />
+                        <Form.Check type="checkbox" label="Eggs" />
+                        <Form.Check type="checkbox" label="Lactose" />
+                        
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Save
+                    </Button>
+                </Form>
             </div>
             )
         }
