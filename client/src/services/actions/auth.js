1ef82503672262664bb8296144b9aafee8de8778
BCT <bertrand.chevalier@gmail.com>
2020-08-25 22:00:51 -0400

    Deleted comments

--- client/src/services/actions/auth.js
+++ client/src/services/actions/auth.js
@@ -13,17 +13,7 @@ export function sign_in(email, password, history){
                 password: password
             })
         })
-        // .then(response => response.json()}
-        // .then(response => response.json())
         .then(response => { 
-        //     // if(!response.ok){ 
-        //     //     if(response.status === 401){
-        //     //         alert("Please verify your login and password")
-        //     //     }else{
-        //     //         alert("Could not log you in, please try again later")
-        //     //     }
-        //     //     throw new Error(response)
-        //     // }else{
             const user = {};
             console.log("response.headers" + response.headers)
             response.headers.forEach((value, name) => user[name] = value);
@@ -32,39 +22,11 @@ export function sign_in(email, password, history){
         })
         .then(response =>{
             if(!response){ 
-                // if(response.status === 401){
-                //     alert("Please verify your login and password")
-                // }else{
-                //     alert("Could not log you in, please try again later")
-                // }
                 throw new Error("no reponse")
             }else{
-                // const user = {};
-                // response.headers.forEach((value, name) => user[name] = value);
-                // localStorage.setItem('user', JSON.stringify(user));
                 dispatch({type:'SIGN_IN_USER_SUCCESS', payload: response.data })
                 history.push('/')
             }
-        // })
-                // let accessToken = response.headers['access-token']
-                // let uid = response.headers['uid']
-                // // let expiry = response.headers['expiry']
-                // let client = response.headers['client']
-      
-                // if ( accessToken && accessToken !== "" ) {
-                //   localStorage.setItem('access-token', accessToken);
-                // }
-                // if ( uid && uid !== "" ) {
-                //   localStorage.setItem('uid', uid);
-                // }
-                // // if ( expiry && expiry !== "" ) {
-                // //   localStorage.setItem('expiry', expiry );
-                // // }
-                // if ( client && client !== "" ) {
-                //   localStorage.setItem('client', client);
-                // } 
-                
-            // }
         })
         .catch(error =>{
             dispatch({type:'SIGN_IN_USER_FAILURE', payload: error, error:true})
@@ -76,10 +38,6 @@ export function verify_credentials(){
     return(dispatch=>{
         const strUser = localStorage.getItem('user')
         let user = JSON.parse(strUser)
-        // const token = localStorage.getItem('access-token');
-        // const uid = localStorage.getItem('uid');
-        // // const expiry = localStorage.getItem('expiry');
-        // const client = localStorage.getItem('client');
         dispatch({type: "CREDENTIAL_VERIFICATION"})
         console.log("user " + user)
         return fetch("http://localhost:3000/api/v1/auth/validate_token", {
@@ -92,11 +50,9 @@ export function verify_credentials(){
         .then(response => response.json())
         .then(response=>{
             console.log(response)
-            // if(!response.success) throw new Error(response.status)
             dispatch({type: "CREDENTIAL_VERIFICATION_SUCCESS", payload: response.data})
         })  
         .catch(error=>{
-            // console.log(error)
             dispatch({type:'CREDENTIAL_VERIFICATION_FAILURE', payload: error, error:true})
         })
     })
